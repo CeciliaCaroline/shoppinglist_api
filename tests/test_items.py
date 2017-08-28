323b2ea59f64ab9a6d4dfea49b66d3fe96ae64c4
CeciliaCaroline <nalubegac58@gmail.com>
2017-08-28 19:57:53 +0300

    Added comments

--- tests/test_items.py
+++ tests/test_items.py
@@ -5,6 +5,9 @@ import json
 
 class ItemsTestCase(BaseTestCase):
     def test_create_item(self):
+        """"
+        test item can be created
+        """
         with self.client:
             token = self.token()
 
@@ -24,6 +27,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertIn('Go_to_Nairobi', response.data.decode())
 
     def test_create_item_with_wrong_content_type(self):
+        """"
+        test item can not be created with wrong content type
+        """
         with self.client:
             token = self.token()
 
@@ -43,6 +49,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertIn('Content-type must be json', response.data.decode())
 
     def test_create_item_with_empty_name(self):
+        """"
+        test item can not be created with empty item name
+        """
         with self.client:
             token = self.token()
 
@@ -62,6 +71,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertIn('No name has been input', response.data.decode())
 
     def test_create_item_with_invalid_name_format(self):
+        """"
+        test item cant be created with invalid item name format
+        """
         with self.client:
             token = self.token()
 
@@ -81,6 +93,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertIn('Wrong name format. Name can only contain letters and numbers', response.data.decode())
 
     def test_create_item_with_string_price(self):
+        """"
+        test item cant be created if input price is a string
+        """
         with self.client:
             token = self.token()
 
@@ -100,6 +115,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertIn('Item price should be an integer', response.data.decode())
 
     def test_get_items(self):
+        """"
+        test API can get items in a list
+        """
         with self.client:
             token = self.token()
 
@@ -117,6 +135,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertEqual(response.status_code, 200)
 
     def test_get_item(self):
+        """"
+        test API can get single item in a list
+        """
         with self.client:
             token = self.token()
 
@@ -139,6 +160,9 @@ class ItemsTestCase(BaseTestCase):
             self.assertEqual(response.status_code, 200)
 
     def test_get_nonexistent_item(self):
+        """"
+        test API cant get non existent item from a list
+        """
         with self.client:
             token = self.token()
 
