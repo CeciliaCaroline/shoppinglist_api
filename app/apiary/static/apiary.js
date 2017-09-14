ecbaf2e889df4ff688e24a874e5aaa34a3a5078e
CeciliaCaroline <nalubegac58@gmail.com>
2017-09-14 12:33:51 +0300

    Added files to the apiary blueprint to enable access to documentation

--- app/apiary/static/apiary.js
+++ app/apiary/static/apiary.js
@@ -1,16 +1,11 @@
 var embed = new Apiary.Embed({
-  "subdomain": "shoppinglist8",
+  "subdomain": "shoppinglist9",
   "preferences": {
-    "element": "#documentation",
-    "displayHttpMethods": true
+    "element": "#documentation"
   },
   "theme": {
     "fontFamily": "Ubuntu",
-    "humanColumn": {
-      "wrapper": {
-        "background": "url(https://help.apiary.io/images/embed/Artboard.png)"
-      }
-    }
+
   },
   "customFonts": {
     "googleFonts": "https://fonts.googleapis.com/css?family=Ubuntu"
