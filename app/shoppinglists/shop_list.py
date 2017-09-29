747ada70037e8bc69bb1a310b43f71bd8da06040
CeciliaCaroline <nalubegac58@gmail.com>
2017-09-30 02:18:25 +0300

    Removed unused variables

--- app/shoppinglists/shop_list.py
+++ app/shoppinglists/shop_list.py
@@ -1,6 +1,5 @@
 from app import db
 from flask import Blueprint, request, make_response, jsonify
-from flask.views import MethodView
 from app.models import Shoppinglist
 from app.authenticate.token import token_required
 import re
